995526e1a9e4cb7e69b2ebfbe53859019925129f
KHALID NICCUR <113862545+Khalid-niccur@users.noreply.github.com>
2023-07-03 01:34:11 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,9 +5,9 @@ const chalk = require('chalk')
 /*
 🤖 𝗕𝗢𝗧 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OFFLINE = false // if you want put bot offline put true
+global.OFFLINE = true // if you want put bot offline put true
 global.BOT_PRIVET = false // if you want use bot privet 
-global.AUTO_CHATGPT = false // if you want auto ai bot mod
+global.AUTO_CHATGPT = true // if you want auto ai bot mod
 global.READ_MESSAGE = false // if you want read message from bot
 global.ONLY_INBOX = false // Only inbox mod ( only pm ) use
 global.ONLY_GROUPS = false // Only Groups Use
@@ -16,9 +16,9 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
-global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers
+global.OWNER_NAME = 'khalid niccur technologies' // Owner Name
+global.OWNER_NUMBER = ['254736958034'] //Owner Number
+global.BOT_ADMINS = ['254736958034','254736958034'] //Bot admins numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 
 
@@ -39,7 +39,7 @@ global.PORT = process.env.PORT || '5000' // Port No need Change.
 
 
 mess = {
-	owner: '*This Command is only For Owner.*'
+	owner: '💙This Command is only For khalid✨ my humble master🤦‍♀️.*'
 }
 
 
