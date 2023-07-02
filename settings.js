a680e2063c9a86e5b77940c5d9cfe75acd7fd46b
KHALID NICCUR <113862545+Khalid-niccur@users.noreply.github.com>
2023-07-03 01:41:37 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,8 +27,8 @@ global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and 
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-global.AI_PASSWORD = ''
-global.SESSION_ID = ''
+global.AI_PASSWORD = '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'
+global.SESSION_ID = 'elisa_md@;;;lF1k0I5J#eeL-WFfZ1bz0xtSfFWcO5GDHJ_Aten44-csixKK2flE'
 
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
